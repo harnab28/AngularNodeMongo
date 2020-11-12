91a325bd4904d671150fe3b17295cadedfd8a36b
Arnab <harnab28@gmail.com>
2020-11-13 00:43:52 +0530

    reCaptcha Module Completed

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,22 +1,22 @@
-import { CommonModule } from '@angular/common'
-import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
-import { AngularMaterialModule } from '../angular-material.module'
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularMaterialModule } from '../angular-material.module';
 
-import { LoginComponent } from './login/login.component'
-import { SignupComponent } from './signup/signup.component'
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+import { RecaptchaModule, RecaptchaFormsModule } from 'ng-recaptcha';
 
 @NgModule({
-    declarations:[
-        LoginComponent,
-        SignupComponent,
-    ],
+  declarations: [LoginComponent, SignupComponent],
 
-    imports: [
-        CommonModule,
-        FormsModule,
-        AngularMaterialModule
-    ]
+  imports: [
+    CommonModule,
+    FormsModule,
+    AngularMaterialModule,
+    RecaptchaFormsModule,
+    RecaptchaModule,
+  ],
 })
-
-export class AuthModule{ }
+export class AuthModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,37 +1,42 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 
-
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
-    
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit , OnDestroy {
-    isLoading: boolean = false;
-    authstatusSubs : Subscription;
-    constructor(public authService: AuthService) {}
+export class LoginComponent implements OnInit, OnDestroy {
+  isLoading: boolean = false;
+  authstatusSubs: Subscription;
+  disableBtn: boolean;
+  constructor(public authService: AuthService) {}
 
-    ngOnInit(){
-        this.authstatusSubs = this.authService.getAuthStatusListner().subscribe((authStatus) => {
-            this.isLoading = false;
-        })
-    }
+  ngOnInit() {
+    this.authstatusSubs = this.authService
+      .getAuthStatusListner()
+      .subscribe((authStatus) => {
+        this.isLoading = false;
+        this.disableBtn = false;
+      });
+  }
 
-    onLogin(form: NgForm){
-        if(form.invalid)
-            return;
+  onLogin(form: NgForm) {
+    if (form.invalid) return;
 
-        this.isLoading = true;
-        this.authService.login(form.value.email, form.value.password);
-        form.resetForm();
-    }
+    this.isLoading = true;
+    this.authService.login(form.value.email, form.value.password);
+    form.resetForm();
+  }
 
-    ngOnDestroy(){
-        this.authstatusSubs.unsubscribe();
-    }
+  resolved(captchaEvent: string) {
+    if (captchaEvent) this.disableBtn = false;
+    else this.disableBtn = true;
+  }
 
+  ngOnDestroy() {
+    this.authstatusSubs.unsubscribe();
+  }
 }
